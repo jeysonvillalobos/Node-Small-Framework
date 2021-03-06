fd2ea5e4af127a77b1179ed30a24e958c221f99e
Jeyson Villalobos <jeyson3213@hotmail.com>
2021-03-05 22:22:15 -0500

    Port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Framework = require('./core/framework');
 
-const server = Framework(80);
+const server = Framework(5000);
 
 server.get('/','Bienvenido a la pagina.');
 server.get('/about','Esta pagina ha sido creada con NodeJS con un framework de prueba.');
